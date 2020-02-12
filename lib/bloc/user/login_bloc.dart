e72ee56bb22840eb12b42b357d1078f9807ee07b
Vico Melo <vinicomelo@gmail.com>
2020-02-11 23:19:34 -0500

    sample state login works

---
+++ lib/bloc/user/login_bloc.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter_login/bloc/user/authentication_bloc.dart';
+import 'package:flutter_login/bloc/user/authentication_event.dart';
+import 'package:flutter_login/bloc/user/login_event.dart';
+import 'package:flutter_login/bloc/user/login_state.dart';
+import 'package:flutter_login/bloc/user/user_repository.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.add(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
